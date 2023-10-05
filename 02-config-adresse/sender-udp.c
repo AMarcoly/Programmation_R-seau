e17260e5e57d419f9094d960c11393e27467de05
Marcoly Antoine <marcolyantoine@yahoo.com>
2023-10-06 01:02:46 +0200

    Commit test exo2

--- 02-config-adresse/sender-udp.c
+++ 02-config-adresse/sender-udp.c
@@ -14,27 +14,44 @@
 int main (int argc, char *argv [])
 {
     //* test arg number */
-//    if( argc != 2){
-//         fprintf(stderr, "Erreur us : %s <port number> \n",argv[0]);
-//         exit(EXIT_FAILURE);
-//     }
+   if( argc != 3){
+        fprintf(stderr, "Erreur us : %s <port number>\n",argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-//     /* convert and check port number */
-//     int port_number = atoi(argv[1]);
-//     if(port_number<10000 || port_number > 65000){
-//         fprintf(stderr,"Mauvais format de numero de port : intervalle [10 000:65 000]\n");
-//         exit(EXIT_FAILURE);
-//     }
+    /* convert and check port number */
+    char * str_port = argv[2];
+    const char * message = "hello world";
 
-    /* create socket */
+    int port_number = atoi(str_port);
+    if(port_number<10000 || port_number > 65000){
+        fprintf(stderr,"Mauvais format de numero de port : intervalle [10 000:65 000]\n");
+        exit(EXIT_FAILURE);
+    }
 
-    /* complete sockaddr struct */
 
-    /* send message to remote peer */
+    int fd_socket;
+    fd_socket=socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK(fd_socket);
 
-    /* close socket */
+    struct addrinfo hints;
+    struct addrinfo *res = NULL;
 
-    /* free memory */
+    hints.ai_family = AF_INET; // IPV4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+
+    int fd_addr = getaddrinfo (IP, str_port, &hints, &res);
+    CHECK(fd_addr);
+
+    if(res==NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    CHECK(sendto(fd_socket,message,strlen(message),0,res->ai_addr,res->ai_addrlen));
+
+    CHECK(close(fd_socket));
+   
+    freeaddrinfo (res);
 
     return 0;
 }
