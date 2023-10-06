150491583ddd794929cdb1d07ff2613441136eae
Marcoly Antoine <marcolyantoine@yahoo.com>
2023-10-06 02:12:14 +0200

    fin exo3

--- 03-recepteurUDPv4/receiver-udp.c
+++ 03-recepteurUDPv4/receiver-udp.c
@@ -15,13 +15,51 @@
 
 int main (int argc, char *argv [])
 {
-    /* test arg number */
+    // test nombres args et intervalles
+    if( argc != 2){
+        fprintf(stderr, "Erreur us : %s <port number>\n",argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    /* convert and check port number */
+    char * str_port = argv[1];
+    char response[SIZE];
 
-    /* create socket */
+    int port_number = atoi(str_port);
+    if(port_number<10000 || port_number > 65000){
+        fprintf(stderr,"Mauvais format de numero de port : intervalle [10 000:65 000]\n");
+        exit(EXIT_FAILURE);
+    }
 
-    /* complete struct sockaddr */
+    // creation de socket
+    int fd_socket;
+    fd_socket=socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK(fd_socket);
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    struct addrinfo *res = NULL;
+
+    hints.ai_family = AF_INET; // IPV4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+
+    int fd_addr = getaddrinfo (IP, str_port, &hints, &res);
+    CHECK(fd_addr);
+
+    if(res==NULL){
+        exit(EXIT_FAILURE);
+    }
+    else{
+        CHECK(bind (fd_socket, res->ai_addr, res->ai_addrlen));
+
+        CHECK(recvfrom(fd_socket, response, sizeof(response), 0, NULL,NULL));
+
+        freeaddrinfo (res); 
+        printf("%s",response);
+
+
+    }
+
+    CHECK(close(fd_socket));
 
     /* link socket to local IP and PORT */
 
