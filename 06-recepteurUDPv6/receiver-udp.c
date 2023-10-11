5ae9c15704c5bdeea82e6e7a26ea3e3ba8fdbc98
Yanis Moulla <yanis.moulla@etu.unistra.fr>
2023-10-11 15:32:59 +0200

    non mais essai

--- 06-recepteurUDPv6/receiver-udp.c
+++ 06-recepteurUDPv6/receiver-udp.c
@@ -15,23 +15,46 @@
 
 int main (int argc, char *argv [])
 {
-    /* test arg number */
+    //* test arg number */
+   if( argc != 2){
+        fprintf(stderr, "Erreur us : %s <port number>\n",argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     /* convert and check port number */
+    char * str_port = argv[1];
+    const char * message = "hello world";
 
-    /* create socket */
+    int port_number = atoi(str_port);
+    if(port_number<10000 || port_number > 65000){
+        fprintf(stderr,"Mauvais format de numero de port : intervalle [10 000:65 000]\n");
+        exit(EXIT_FAILURE);
+    }
 
-    /* complete struct sockaddr */
 
-    /* link socket to local IP and PORT */
+    int fd_socket;
+    fd_socket=socket(AF_INET6, SOCK_DGRAM, 0);
+    CHECK(fd_socket);
 
-    /* wait for incoming message */
-    
-    /* print sender addr and port */
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    struct addrinfo *res = NULL;
 
-    /* close socket */
+    hints.ai_family = AF_INET6; // IPV6
+    hints.ai_socktype = SOCK_DGRAM; // UDP
 
-    /* free memory */
+    int fd_addr = getaddrinfo (IP, str_port, &hints, &res);
+    CHECK(fd_addr);
+
+    if(res==NULL){
+        exit(EXIT_FAILURE);
+    }
+    else{
+        CHECK(sendto(fd_socket,message,strlen(message),0,res->ai_addr,res->ai_addrlen));
+        freeaddrinfo (res);    
+    }
+
+    CHECK(close(fd_socket));
 
     return 0;
 }
