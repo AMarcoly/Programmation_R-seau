d4bf2f84c68e33d30e13ddd7956ca849459f4cfb
Marcoly Antoine <marcolyantoine@wifi-eduroam-interne-172-29-6-220.wifi.unistra.fr>
2023-10-31 13:14:57 +0100

    test 1

--- 09-transfert-fichierTCPv6-source/sender-tcp.c
+++ 09-transfert-fichierTCPv6-source/sender-tcp.c
@@ -16,30 +16,68 @@
 
 void cpy (int src, int dst)
 {
-    return;
+    char buffer[SIZE];
+    ssize_t bytes_read;
+    int n;
+
+    while ((bytes_read = read(src, buffer, SIZE)) > 0)
+    {
+        n=write(dst, buffer, bytes_read);
+        CHECK(n);
+    }
+    CHECK(bytes_read);
 }
 
 int main (int argc, char *argv [])
 {
-    /* test arg number */
-
+     /* test arg number */
+    if( argc != 4){
+        fprintf(stderr, "usage: %s ip_addr port_number\n",argv[0]);
+        exit(EXIT_FAILURE);
+    }
     /* convert and check port number */
-
+    int port_number = atoi(argv[2]);
+    if(port_number<10000 || port_number > 65000){
+        fprintf(stderr,"Mauvais format de numero de port : intervalle [10 000:65 000]\n");
+        exit(EXIT_FAILURE);
+    }
     /* open file to send */
-
+    int fd = open(argv[3],O_RDONLY);
+    CHECK(fd);
     /* create socket */
-
+    int sockfd;
+    sockfd=socket(AF_INET, SOCK_STREAM, 0);
+    CHECK(sockfd);
     /* complete struct sockaddr */
+    
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    struct addrinfo *res = NULL;
 
+    hints.ai_family = AF_INET6; // IPV6
+    hints.ai_socktype = SOCK_STREAM; //TCP
+    hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV; 
+
+    int fd_addr = getaddrinfo (argv[1], str_port, &hints, &res);
+    if (fd_addr != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(fd_addr));
+        exit(EXIT_FAILURE);
+    }
+
+    if(res==NULL){
+        exit(EXIT_FAILURE);
+    }
     /* connect to the remote peer */
-
+     /* connect to server */
+    CHECK(connect(fd_socket,res->ai_addr,res->ai_addrlen));
     /* send the file content */
-
+    cpy(fd,fd_socket);
     /* close socket */
-
+    CHECK(close(fd_socket));
     /* close file */
-
+    CHECK(close(fd));
     /* free memory */
+    freeaddr(res);
 
     return 0;
 }
