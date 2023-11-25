54feb0397c181027531471fc4bfff22f1e4a72e9
Hysons Moustellier <marcolyzephir15@gmail.com>
2023-11-25 13:25:48 +0100

    test valg

--- 12-chat/tests.sh
+++ 12-chat/tests.sh
@@ -24,19 +24,19 @@ R=$?
 [ "$R" != "0" ]   && echo "KO -> exit status $R instead of 0" && exit 1
 echo "...OK"
 
-# echo -n "test 04 - program in client mode: "
-# echo -n "/QUIT" > cmd.tmp
-# timeout 5 nc -6ul ::1 $PORT > output & 
-# TO=$!
-# sleep 2
-# timeout 3 $PROG $PORT < cmd.tmp
-# R=$?
-# [ "$R" == "124" ] && echo "KO -> programs times out"          && exit 1
-# [ "$R" != "0" ]   && echo "KO -> exit status $R instead of 0" && exit 1
-# wait $TO
-# MES=`cat output`
-# [ "$MES" != "/HELO/QUIT" ] && echo "KO -> program sent $MES instead /HELO/QUIT" && exit 1
-# echo "...............OK"
+echo -n "test 04 - program in client mode: "
+echo -n "/QUIT" > cmd.tmp
+timeout 5 nc -6ul ::1 $PORT > output & 
+TO=$!
+sleep 2
+timeout 3 $PROG $PORT < cmd.tmp
+R=$?
+[ "$R" == "124" ] && echo "KO -> programs times out"          && exit 1
+[ "$R" != "0" ]   && echo "KO -> exit status $R instead of 0" && exit 1
+wait $TO
+MES=`cat output`
+[ "$MES" != "/HELO/QUIT" ] && echo "KO -> program sent $MES instead /HELO/QUIT" && exit 1
+echo "...............OK"
 
 echo -n "test 05 - program accept IPv4 clients: "
 timeout 5 $PROG $PORT > output 2> /dev/null &
